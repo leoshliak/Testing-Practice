7cd8f2045af45fcd9d71b7098b47516d1cb4543b
leoshliak <levshlyak@gmail.com>
2024-08-02 14:16:12 +0300

    caesarCipher completed

---
+++ caesarCipher.js
@@ -0,0 +1,38 @@
+function caesarCipher(str, shift) {
+  const codeA = 'A'.charCodeAt(0);
+  const codeZ = 'Z'.charCodeAt(0);
+  const codea = 'a'.charCodeAt(0);
+  const codez = 'z'.charCodeAt(0);
+
+  let result = '';
+
+  for (let chr of str) {
+    let code = chr.codePointAt(0);
+
+    if (code >= codeA && code <= codeZ) {
+      code -= codeA;
+      code = mod(code + shift, 26);
+      code += codeA;
+    } else if (code >= codea && code <= codez) {
+      code -= codea;
+      code = mod(code + shift, 26);
+      code += codea;
+    }
+
+    result += String.fromCodePoint(code);
+  }
+
+  return result;
+}
+
+function mod(n, p) {
+  let r = n % p;
+
+  if (r < 0) {
+    r += p;
+  }
+
+  return r;
+}
+
+module.exports = {caesarCipher, mod};
---
+++ caesarCipher.test.js
@@ -0,0 +1,13 @@
+const {caesarCipher, mod} = require('./caesarCipher');
+
+ test('wrapping from z to a test ', () => {
+   expect(caesarCipher('xyz', 3)).toBe('abc');
+ });
+
+ test('case preservation test', () => {
+    expect(caesarCipher('HeLLo', 3)).toBe('KhOOr');
+  });
+
+  test('punctuation test', () => {
+    expect(caesarCipher('Hello, World!', 3)).toBe('Khoor, Zruog!');
+  });
